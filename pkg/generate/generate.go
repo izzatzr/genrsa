0e0f4b06aed8d64792074633f681a3e19fb6dc78
izzatzr <izzatzr@outlook.com>
2021-08-17 04:50:42 +0700

    rewrite

--- pkg/generate/generate.go
+++ pkg/generate/generate.go
@@ -12,64 +12,101 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+type Opts struct {
+	PrivateKey PrivateKey
+	PublicKey  PublicKey
+}
+
+type PrivateKey struct {
+	BitSize int
+	Path    string
+	Blob    Blob
+}
+
+type PublicKey struct {
+	Blob Blob
+	Path string
+}
+
+type Blob struct {
+	File *os.File
+	Data []byte
+}
+
 // Create generate temporary public & private RSA Key
-func Create() (os.File, os.File, error) {
+func (opts *Opts) Create() error {
+	var (
+		err error
+	)
 
-	pvKeyFile, err := ioutil.TempFile(os.TempDir(), "*")
+	if opts.PrivateKey.Path == "" {
+		opts.PrivateKey.Blob.File, err = ioutil.TempFile(os.TempDir(), "PrivateKey-*")
+		if err != nil {
+			return errors.Wrap(err, err.Error())
+		}
+
+		opts.PrivateKey.Path = opts.PrivateKey.Blob.File.Name()
+	}
+
+	if opts.PublicKey.Path == "" {
+		opts.PublicKey.Blob.File, err = ioutil.TempFile(os.TempDir(), "PublicKey-*.pub")
+		if err != nil {
+			return errors.Wrap(err, err.Error())
+		}
+
+		opts.PublicKey.Path = opts.PublicKey.Blob.File.Name()
+	}
+
+	key, err := opts.privateKey(opts.PrivateKey.BitSize)
 	if err != nil {
-		return os.File{}, os.File{}, errors.Wrap(err, err.Error())
+		return errors.Wrap(err, err.Error())
 	}
 
-	pbKeyFile, err := ioutil.TempFile(os.TempDir(), "*.pub")
+	err = opts.publicKey(&key.PublicKey)
 	if err != nil {
-		return os.File{}, os.File{}, errors.Wrap(err, err.Error())
+		return errors.Wrap(err, err.Error())
 	}
 
-	key, pvKeyData, err := privateKey()
-	if err != nil {
-		return os.File{}, os.File{}, errors.Wrap(err, err.Error())
+	for _, b := range []Blob{
+		opts.PrivateKey.Blob,
+		opts.PublicKey.Blob,
+	} {
+		if err = ioutil.WriteFile(b.File.Name(), b.Data, os.ModePerm); err != nil {
+			return errors.Wrap(err, err.Error())
+		}
+		defer b.File.Close()
 	}
 
-	pbKeyData, err := publicKey(&key.PublicKey)
-	if err != nil {
-		return os.File{}, os.File{}, errors.Wrap(err, err.Error())
-	}
-
-	if err = ioutil.WriteFile(pvKeyFile.Name(), pvKeyData, os.ModePerm); err != nil {
-		return os.File{}, os.File{}, errors.Wrap(err, err.Error())
-	}
-
-	if err = ioutil.WriteFile(pbKeyFile.Name(), pbKeyData, os.ModePerm); err != nil {
-		return os.File{}, os.File{}, errors.Wrap(err, err.Error())
-	}
-
-	return *pvKeyFile, *pbKeyFile, nil
+	return nil
 }
 
-func privateKey() (*rsa.PrivateKey, []byte, error) {
-	key, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = key.Validate()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return key, encodePrivateKey(key), nil
-}
-
-func publicKey(pvKey *rsa.PublicKey) ([]byte, error) {
-	key, err := ssh.NewPublicKey(pvKey)
+func (opts *Opts) privateKey(bitsize int) (*rsa.PrivateKey, error) {
+	key, err := rsa.GenerateKey(rand.Reader, bitsize)
 	if err != nil {
 		return nil, err
 	}
 
-	keyBytes := ssh.MarshalAuthorizedKey(key)
-	return keyBytes, nil
+	err = key.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	opts.PrivateKey.Blob.Data = opts.encodePrivateKey(key)
+
+	return key, nil
 }
 
-func encodePrivateKey(pvKey *rsa.PrivateKey) []byte {
+func (opts *Opts) publicKey(pvKey *rsa.PublicKey) error {
+	key, err := ssh.NewPublicKey(pvKey)
+	if err != nil {
+		return err
+	}
+
+	opts.PublicKey.Blob.Data = ssh.MarshalAuthorizedKey(key)
+
+	return nil
+}
+
+func (opts *Opts) encodePrivateKey(pvKey *rsa.PrivateKey) []byte {
 	return pem.EncodeToMemory(&pem.Block{Bytes: x509.MarshalPKCS1PrivateKey(pvKey)})
 }
